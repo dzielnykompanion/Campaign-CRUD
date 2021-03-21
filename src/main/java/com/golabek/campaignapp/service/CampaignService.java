78708ae111bf0381dda4313dde6aa77729e83186
mglbt <golabek.mat@gmail.com>
2021-03-21 13:53:08 +0100

    campaign service

---
+++ src/main/java/com/golabek/campaignapp/service/CampaignService.java
@@ -0,0 +1,35 @@
+package com.golabek.campaignapp.service;
+
+import com.golabek.campaignapp.model.Campaign;
+import com.golabek.campaignapp.repository.CampaignRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CampaignService {
+
+    @Autowired
+    private CampaignRepo campaignRepo;
+
+    public Campaign addOrUpdateCampaign(Campaign campaign){
+        //TODO
+        return campaignRepo.save(campaign);
+    }
+
+    public List<Campaign> getAll(){
+        return campaignRepo.findAll();
+    }
+
+    public Campaign findById(Long id){
+        //TODO
+        return campaignRepo.findById(id).get();
+    }
+
+    public void deleteCampaign(Long id){
+        //TODO
+        campaignRepo.deleteById(id);
+    }
+
+}
