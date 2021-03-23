f03f8d67e1aaf7f3c59a79c2cf18643cc14a6bc4
mglbt <golabek.mat@gmail.com>
2021-03-23 19:38:05 +0100

    campaign details view

--- campaign-app-angular-client/src/app/app.module.ts
+++ campaign-app-angular-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CreateCampaignComponent } from './create-campaign/create-campaign.component';
@@ -11,6 +12,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { CampaignsListComponent } from './campaigns-list/campaigns-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
+
 
 @NgModule({
   declarations: [
@@ -25,7 +29,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
